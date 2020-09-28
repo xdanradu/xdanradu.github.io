f46eeedc8be3a93efb36697bb6a6f2c94c1cede0
Dan Radu <dan.radu@msg.group>
2020-09-28 16:55:16 +0300

    test1

---
+++ test1/precache-manifest.4bade173171941b5e4b975d019f0a647.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c89e4c0fb83cc1027c87897dd0169e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0f5e5fd4488000c480",
+    "url": "/static/css/main.6577e6c3.chunk.css"
+  },
+  {
+    "revision": "a2ec560e4a2fbbdd6716",
+    "url": "/static/js/2.63e3edfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63e3edfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0f5e5fd4488000c480",
+    "url": "/static/js/main.0b6ee49c.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "309040b6e57cb05a6863228a4d395521",
+    "url": "/static/media/Oswald-ExtraLight.309040b6.ttf"
+  },
+  {
+    "revision": "4be970a1b24aa834afff4b16842c8981",
+    "url": "/static/media/Oswald-Light.4be970a1.ttf"
+  }
+]);
