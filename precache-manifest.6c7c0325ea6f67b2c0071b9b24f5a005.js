0beaffaccf1bec6eb16337da03e84695c248a10e
Dan Radu <dan.radu@msg.group>
2020-09-29 13:51:00 +0300

    a

---
+++ precache-manifest.6c7c0325ea6f67b2c0071b9b24f5a005.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfd3e5891cf3e401886021de66379f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d915aa1196cf5988213a",
+    "url": "/static/css/main.90987851.chunk.css"
+  },
+  {
+    "revision": "4dc12b327144541f1971",
+    "url": "/static/js/2.7f14598e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f14598e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d915aa1196cf5988213a",
+    "url": "/static/js/main.1f3614c6.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "309040b6e57cb05a6863228a4d395521",
+    "url": "/static/media/Oswald-ExtraLight.309040b6.ttf"
+  },
+  {
+    "revision": "4be970a1b24aa834afff4b16842c8981",
+    "url": "/static/media/Oswald-Light.4be970a1.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8c54e9473f65af9b1ed3e8710f80a7b.js"
+  "/precache-manifest.6c7c0325ea6f67b2c0071b9b24f5a005.js"
 );
 
 self.addEventListener('message', (event) => {
