83451e6f6de503f3dc388994a122bf72ab19bb09
Dan Radu <xdanradu@yahoo.com>
2020-09-29 22:34:01 +0300

    card

--- app/src/components/about/about.jsx
+++ app/src/components/about/about.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import "./about.scss";
 
 class About extends Component {
     render() {
         return (
-        <div>About</div>
+            <div className="container">
+                <div className="card">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <div className="content">
+                        <h2>01</h2>
+                        <h3>Fullstack</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. </p>
+                        <a href="#">Read more</a>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
---
+++ precache-manifest.722d0589fee5ccefc8c5143bb4406385.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cdc69743052fde4391dd04b0604b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c86a6ab1a747f94bf3e",
+    "url": "/static/css/main.bf3b9a9d.chunk.css"
+  },
+  {
+    "revision": "284d230f1f76eabaee9b",
+    "url": "/static/js/2.05576885.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.05576885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c86a6ab1a747f94bf3e",
+    "url": "/static/js/main.c2b0bd46.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "309040b6e57cb05a6863228a4d395521",
+    "url": "/static/media/Oswald-ExtraLight.309040b6.ttf"
+  },
+  {
+    "revision": "4be970a1b24aa834afff4b16842c8981",
+    "url": "/static/media/Oswald-Light.4be970a1.ttf"
+  }
+]);
