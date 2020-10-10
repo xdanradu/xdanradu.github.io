eb11d89ddbe55cb3a27a8220041429729f58fbef
Dan Radu <xdanradu@yahoo.com>
2020-10-10 14:51:31 +0300

    Automatic PUSH all changes on: 2020-10-10_14-50-07

--- app/deploy.bat
+++ app/deploy.bat
@@ -3,6 +3,8 @@ echo "COPY the build to the root folder xdanradu.github.io"
 cd ..
 SET PARENT_FOLDER=%cd%
 ECHO %PARENT_FOLDER%
+set_date.bat
+echo %CURRENT_DATE%
 del *.* /Q
 rmdir "static" /S /Q
 cd app
@@ -10,6 +12,5 @@ xcopy /s build %PARENT_FOLDER% /Q
 echo "COMMIT all changes and PUSH them to github repo"
 cd ..
 git add .
-date.bat
 git commit -m "Automatic PUSH all changes on: "%CURRENT_DATE%
 git push
