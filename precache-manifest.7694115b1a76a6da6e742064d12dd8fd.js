d8bd2b8e8a154e6fd7f88c5961b4ac0596c35293
Dan Radu <xdanradu@yahoo.com>
2023-06-28 10:42:11 +0300

    Automatic PUSH all changes on:

---
+++ precache-manifest.7694115b1a76a6da6e742064d12dd8fd.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1648bfa20af6c797b47e0b30fcae36ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60ac2f729a06f4834ab5",
+    "url": "/static/css/3.263321df.chunk.css"
+  },
+  {
+    "revision": "8a2242a1bdbde07f44d0",
+    "url": "/static/css/4.4599ba3f.chunk.css"
+  },
+  {
+    "revision": "c17b7fea1b65f176c0cb",
+    "url": "/static/css/main.5f0a7e61.chunk.css"
+  },
+  {
+    "revision": "727bdc16eac992f11bc4",
+    "url": "/static/js/2.e4ca7bc2.chunk.js"
+  },
+  {
+    "revision": "78df4f9c4aa6333344e44bde8267630b",
+    "url": "/static/js/2.e4ca7bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ac2f729a06f4834ab5",
+    "url": "/static/js/3.7a2a168a.chunk.js"
+  },
+  {
+    "revision": "8a2242a1bdbde07f44d0",
+    "url": "/static/js/4.f9e37d9f.chunk.js"
+  },
+  {
+    "revision": "c257767a763140de273d",
+    "url": "/static/js/5.2f8a247b.chunk.js"
+  },
+  {
+    "revision": "c17b7fea1b65f176c0cb",
+    "url": "/static/js/main.8899e333.chunk.js"
+  },
+  {
+    "revision": "4e9ca932204394135ed5",
+    "url": "/static/js/runtime-main.3474ae8c.js"
+  },
+  {
+    "revision": "309040b6e57cb05a6863228a4d395521",
+    "url": "/static/media/Oswald-ExtraLight.309040b6.ttf"
+  },
+  {
+    "revision": "4be970a1b24aa834afff4b16842c8981",
+    "url": "/static/media/Oswald-Light.4be970a1.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "22da4d47dc9ffa3fea6fab017292d7b1",
+    "url": "/static/media/angular.22da4d47.png"
+  },
+  {
+    "revision": "b368c9f9e36bc0b0283923e0a1c591bf",
+    "url": "/static/media/dan.b368c9f9.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "4fbb104a96ed9f5379c441825b0eb041",
+    "url": "/static/media/fa-brands-400.4fbb104a.svg"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "4d09b9825b733bd187a22829720f82e3",
+    "url": "/static/media/logo-large-green.4d09b982.svg"
+  },
+  {
+    "revision": "d25e4c2c13c0f4680b9d16646dee0059",
+    "url": "/static/media/logo-medium-green.d25e4c2c.svg"
+  },
+  {
+    "revision": "0ba484fdfc921f9738f2c58a736e3807",
+    "url": "/static/media/logo-small-green.0ba484fd.svg"
+  }
+]);
