6fd4a0d67cd5c9e41a10cb9fa011369fb1c4af2a
Dan Radu <xdanradu@yahoo.com>
2023-07-12 11:32:47 +0300

    Automatic PUSH all changes on:

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Tutorials Introduction - 5min ⏱️
           </Link>
         </div>
       </div>
