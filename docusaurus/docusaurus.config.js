6b997845df9dc637f2dc72de59bab45e6f23c497
Dan Radu <xdanradu@yahoo.com>
2023-10-20 16:36:34 +0300

    Automatic PUSH all changes on:

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'DR',
+        title: 'drd',
         logo: {
           alt: 'drd Site Logo',
           src: 'img/logo192.png',
